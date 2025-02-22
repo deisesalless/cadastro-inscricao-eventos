72f455d579adb0df326fea79fb9af3f449443e3a
deisesalless <deisesalles18@gmail.com>
2025-02-22 16:27:05 -0300

    2.0 criação da classe model subscription

---
+++ src/main/java/br/com/oquefazerembsb/events/model/Subscription.java
@@ -0,0 +1,22 @@
+package br.com.oquefazerembsb.events.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "table_subscription")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscription_number")
+    private Integer subscriptionNumber;
+
+    @Column
+    private EventModel event;
+
+    private UserModel subscriptionID;
+
+    private UserModel indicationID;
+}
