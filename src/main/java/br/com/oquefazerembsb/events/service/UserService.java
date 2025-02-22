b5d2ef2ae6fabd73cffe1f8c696ff202fabb3b37
deisesalless <deisesalles18@gmail.com>
2025-02-22 17:39:07 -0300

    3.0 padronização do request e response do user sendo UserRequestDTO e UserResponseDTO, incluindo a dependencia mapstruct e criado a classe UserMap para converções, criado os métodos save new user + get by id na classe UserService

--- src/main/java/br/com/oquefazerembsb/events/service/UserService.java
+++ src/main/java/br/com/oquefazerembsb/events/service/UserService.java
@@ -1,5 +1,9 @@
 package br.com.oquefazerembsb.events.service;
 
+import br.com.oquefazerembsb.events.dto.UserRequestDTO;
+import br.com.oquefazerembsb.events.dto.UserResponseDTO;
+import br.com.oquefazerembsb.events.mapper.UserMapper;
+import br.com.oquefazerembsb.events.model.UserModel;
 import br.com.oquefazerembsb.events.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +13,14 @@ public class UserService {
 
     @Autowired
     private UserRepository repository;
+
+    public UserResponseDTO saveNewUser(UserRequestDTO dto) {
+        UserModel userModel = repository.save(UserMapper.INSTANCE.userDTOtoUserModel(dto));
+        return UserMapper.INSTANCE.userModelToUserDTO(userModel);
+    }
+
+    public UserResponseDTO getById(Integer id) {
+        UserModel userModel = repository.findById(id).get();
+        return UserMapper.INSTANCE.userModelToUserDTO(userModel);
+    }
 }
